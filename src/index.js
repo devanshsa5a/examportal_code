5e611bbaef4e6f99e32b62e36f448ba0f854d76e
devanshsa5a <mail2devansh@gmail.com>
2020-09-12 18:33:04 +0530

    .

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import questionReducer from './store/reducer/question';
+import getQuestionReducer from './store/reducer/getQuestion';
+import videoReducer from './store/reducer/video';
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = combineReducers({
+  quesNo: questionReducer,
+  questBase: getQuestionReducer,
+  video: videoReducer
+});
+console.log = console.warn = console.error = () => {};
+
+const store = createStore(rootReducer, composeEnhancers(
+  applyMiddleware(thunk)
+));
+
+const app = (
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root')
+);
+
+serviceWorker.unregister();
